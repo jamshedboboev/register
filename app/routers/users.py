9a0e394fa6244ec730a4e9641dc5a6d9f064aed7
Jamshed <jamikaki002@gmail.com>
2026-04-30 04:15:18 +0500

    add users routes "register_user" and login_user add file security with func for hashing passwords

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,6 +1,10 @@
+import bcrypt
 from fastapi import APIRouter
+
+from app.core.connection import db_con
+
 from app.models import UserRegist , UserLogIn
-from app.connection import db_con
+from app.security import get_hashed_pass
 
 
 router = APIRouter(tags=["Users"])
@@ -12,41 +16,63 @@ async def get_user():
     SELECT *
     FROM users
     """
-
     r = await db_con.execute(query)
 
     return r
 
 
-# fix: Добавить роут для авторизации
 @router.post("/users/register")
 async def register_user(user: UserRegist):
+    # fix: проверку на наличие совпадений в БД переместить в файл validate.py
     query = """
-    SELECT *
+    SELECT id, login, email
     FROM users
-    WHERE username = %s OR email = %s
+    WHERE login = %s and email = %s
     """
-    r = await db_con.execute(query, (user.username, user.email))
+    r = await db_con.execute(query, (user.login, user.email))
     if r:
         return {"message": "User already exists"}
 
+    # fix: добавление пользователя в БД переместить в файл связанный с update-ами
     query = """
-    INSERT INTO users (username, email, password)
-    VALUES (%s, %s, %s)
+    INSERT INTO users (login, password_hash, name, surname, birthdate, email)
+    VALUES (%(login)s, %(password)s, %(name)s, %(surname)s, %(birthdate)s, %(email)s)
     """
-    await db_con.execute(query, (user.username, user.email, user.password.get_secret_value()))
+
+    hashed_pass = get_hashed_pass(user.password.get_secret_value())
+
+    await db_con.execute(query, {
+        "login": user.login,
+        "password": hashed_pass,
+        "name": user.name,
+        "surname": user.surname,
+        "birthdate": user.birthdate,
+        "email": user.email
+    })
     return {"message": "User registered successfully"}
 
-# fix: Добавить роут для авторизации
+
 @router.post("/users/login")
 async def login_user(user: UserLogIn):
     query = """
-    SELECT *
+    SELECT id, login, password_hash
     FROM users
-    WHERE username = %s AND password = %s
+    WHERE login = %s
     """
-    r = await db_con.execute(query, (user.username, user.password))
-    if r:
+
+    r = await db_con.execute(query, (user.login,))
+
+    # fix: Доработать данное условие и добавить логиврование и обработку ошибок
+    if not r:
+        raise Exception()
+    
+    # fix: Проверку соответствия пароля переместить в файл validate.py
+    password_check: bool = bcrypt.checkpw(
+        user.password.get_secret_value().encode("utf-8"),
+        r["password_hash"].encode("utf-8") # type: ignore | fix: Добавить в возврат функции execute аннотацию типов
+    )
+
+    if password_check:
         return {"message": "Login successful"}
     else:
         return {"message": "Invalid credentials"}
